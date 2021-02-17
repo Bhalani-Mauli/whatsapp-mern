8b5c18c97d214c5c2c2ab1e5ebefef1002279887
maulibhalani12 <maulipatel12@outlook.com>
2021-02-17 22:55:21 +0530

    completed sidebarpart

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,10 +1,39 @@
+import { Avatar, IconButton } from '@material-ui/core';
 import React from 'react';
 import "./Chat.css";
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Chat() {
     return (
-        <div>
-            <h1>I am Chat</h1>
+        <div className="chat">
+            <div className="chat_header">
+                <Avatar />
+                <div className="chat_headerInfo">
+                    <h3>Dance Room</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat_headerInfo">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat_body">
+                <p>
+                    <span className="chat_name">Mauli</span>
+                    This is a message
+                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
         </div>
     )
 }
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,6 +4,8 @@ import "./Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
@@ -22,8 +24,21 @@ function Sidebar() {
                     </IconButton>
                 </div>
             </div>
+
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchIcon />
+                    <input placeholder = "search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar_chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
         </div>
-    )
+    );
 }
 
 export default Sidebar;
